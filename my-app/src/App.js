35af3a65f485aabaaa17582ea12812cafebc4a21
Eric Emery <ericemery099@gmail.com>
2023-07-06 10:26:13 -0400

    updated hooks in app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,16 @@ import { useState, useEffect } from 'react';
 
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+  const [tasks, setTasks] = useState(() => {
+    return JSON.parse(localStorage.getItem("ToDo")) || [];
+  }); //Checks local storage for items to render, returns a blank array if local storage is empty
+  
+  const [status, setStatus] = useStat(To-Do)
+  useEffect(() => { localStorage.setItem("ToDo", JSON.stringify(ToDo));
+}, [ToDo]); // Tells browser to update local storage when ToDo changes, makes items a string so they can be stored
+
+return (
+
+);
 }
+
